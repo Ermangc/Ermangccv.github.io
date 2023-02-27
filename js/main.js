253d824b9fcb5325f105141886b7c851210aca04
Ermangc <94400187+Ermangc@users.noreply.github.com>
2023-02-27 15:13:30 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let elemnto = document.getElementById("div-oculto");
+
+function mostrarInfo(){
+    elemnto.style.display = "block";
+}
+function cerrarInfo(){
+    elemnto.style.display = "none";
+}
